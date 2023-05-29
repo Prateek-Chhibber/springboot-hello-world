c073bc6984aa6b3f4d6eb761f189cf1bc74d3bf2
prateekchhibber <0392pc@gmail.com>
2023-05-29 15:53:41 +0530

    Springboot POST REST Api with PostMapping and RequestBody

--- src/main/java/net/learner/springboot/controller/StudentController.java
+++ src/main/java/net/learner/springboot/controller/StudentController.java
@@ -1,10 +1,8 @@
 package net.learner.springboot.controller;
 
 import net.learner.springboot.bean.Student;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -39,6 +37,17 @@ public class StudentController {
     @GetMapping("students/query")
     public Student studentRequestVariable(@RequestParam int id, @RequestParam String firstName, @RequestParam String lastName){
         return new Student(id, firstName,lastName);
+    }
+
+//    Spring boot REST Api that handles post request
+//    @PostMapping and @RequestBody
+    @PostMapping("students/create")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Student createStudent(@RequestBody Student student){
+        System.out.println(student.getId());
+        System.out.println(student.getFirstName());
+        System.out.println(student.getLastName());
+        return student;
     }
 
 }
