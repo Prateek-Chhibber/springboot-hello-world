fdd1f8cdf9ced1b64911026a55e1faf83183d220
prateekchhibber <0392pc@gmail.com>
2023-05-29 14:39:46 +0530

    Springboot REST Api with Path Variable

--- src/main/java/net/learner/springboot/controller/StudentController.java
+++ src/main/java/net/learner/springboot/controller/StudentController.java
@@ -2,6 +2,7 @@ package net.learner.springboot.controller;
 
 import net.learner.springboot.bean.Student;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -23,6 +24,13 @@ public class StudentController {
         students.add(new Student(2,"Vishtu","Kothari"));
         students.add(new Student(3,"Prateek","Chhibber"));
         return students;
+    }
+
+//    Spring boot REST Api with Path Variable
+//    {id} - URI template variable
+    @GetMapping("students/{id}")
+    public Student studentPathVariable(@PathVariable("id") int id){
+        return new Student(id,"Tingu", "Pingu");
     }
 
 }
